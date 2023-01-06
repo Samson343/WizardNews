5701cc5a4008c38c7b5626b80414a0b6cdbd7713
jcatlanta <jcatlanta@gmail.com>
2023-01-05 21:33:24 -0500

    routes, and more routes

--- PairExercise.Wizard-news-pt1/app.js
+++ PairExercise.Wizard-news-pt1/app.js
@@ -5,6 +5,9 @@ const postBank = require("./postBank")
 
 const PORT = 1337;
 
+app.use(express.static('public'))
+
+
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
@@ -13,18 +16,60 @@ app.use(morgan('dev'))
 
 app.get("/", (req, res) => {
   const posts = postBank.list()
-
   const html = `<!DOCTYPE html>
   <html>
   <head>
-    <title>Wizard News </title>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
   </head>
   <body>
-    <ul>
-      ${posts.map(post => `<li>${post.title}</li>`)}
-    </ul>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+          <a href="/posts/${post.id}">${post.title}</a>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
   </body>
-  </html>`
+</html>`
 
   res.send(html)
+});
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const posts = postBank.list()
+  res.send(`<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.content}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`);
 });
